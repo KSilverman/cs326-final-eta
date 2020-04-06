6217d6acc903ba65e485636926233c2654b9e2e0
KSilverman <ksilverman@umass.edu>
2020-04-06 19:07:47 -0400

    heroku setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const headerText = { 
+    "Content-Type": "application/json", 
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*"
+};
+let server = http.createServer();
+server.on('request', async (request, response) => {
+    response.writeHead(200, headerText);
+});
+ 
+server.listen(process.env.PORT);
