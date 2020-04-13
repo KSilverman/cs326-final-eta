acaa740b6e9bcd52de6d20597e93a20271cefdfd
Devon Endicott <dephurites@gmail.com>
2020-04-13 17:44:32 -0400

    more stubbing

---
+++ types/user.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var User = /** @class */ (function () {
+    function User(id, username) {
+        this.id = id;
+        this.username = username;
+    }
+    return User;
+}());
+exports.User = User;
--- types/user.ts
+++ types/user.ts
@@ -1,3 +1,9 @@
 export class User {
-  
+  private id : number;
+  private username : string;
+
+  constructor(id: number, username: string) {
+    this.id = id;
+    this.username = username;
+  }
 }
