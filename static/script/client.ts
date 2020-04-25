8decc4e5186c1a498ca05f6730e1ce8051cbadcc
Devon Endicott <dephurites@gmail.com>
2020-04-24 20:13:51 -0400

    assignment list

--- static/script/client.ts
+++ static/script/client.ts
@@ -77,6 +77,67 @@ async function updateCalendar() : Promise<void> {
   calendar.render();
 }
 
+async function updateAssignments() : Promise<void> {
+  var resp = await postData('/user/' + userId + '/assignment/all', '');
+  var obj = await resp.json();
+
+  if (obj.status != 'success') {
+    console.error('Failed to poll assignments');
+    return;
+  }
+
+  console.log(obj.status)
+
+  console.log(obj)
+
+  var categories = obj.categories;
+
+  var html : string = '';
+
+  for (var category of categories) {
+    var title : string = category.title;
+    var assignments = category.assignments;
+
+    html += '<div class="row"> <div class="col-sm h1 py-1 text-center">';
+    html += title;
+    html += '</div></div>';
+
+    html += '<div class="row"><div class="col-xl outline table-responsive"><table class="table"><thead><tr>';
+    html += '<th scope="col">#</th>'
+    html += '<th scope="col">Name</th>'
+    html += '<th scope="col">Class</th>'
+    html += '<th scope="col">Due Date</th>'
+    html += '<th scope="col">Expected TTC</th>'
+    html += '<th scope="col">Notes</th>'
+    html += '<th scope="col">Actions</th>'
+    html += '</tr></thead><tbody>';
+
+    for (var assignment of assignments) {
+      var assignmentTitle : string = assignment.title;
+      var course : string = assignment.course.title;
+      var classId : string = assignment.course.id;
+      var dueDate : string = assignment.dueDate;
+      var expectedTTC : string = assignment.expectedTTC;
+      var notes : string = assignment.notes;
+
+      html += '<tr class="table-danger">';
+      html += '<th scope="row">1</th>'
+      html += '<td>' + assignmentTitle + '</td>'
+      html += '<td>' + course + '</td>'
+      html += '<td>' + dueDate + '</td>'
+      html += '<td>' + expectedTTC + '</td>'
+      html += '<td>' + notes + '</td>'
+      html += '<td><button type="button" class="btn btn-success btn-sm" title="Completed">&#10004;</button><button type="button" class="btn btn-primary btn-sm" title="Edit">&#9997;</button><button type="button" class="btn btn-danger btn-sm" title="Remove">&#10006;</button></td></tr>';
+    }
+
+    html += '</tbody></table></div></div>';
+  }
+
+
+  let assignmentContainer = document.getElementById("assignments") as HTMLElement;
+  assignmentContainer.innerHTML = html;
+}
+
 // NEW: helper method for posting data
 async function postData(url : string, data : any) {
     const resp = await fetch(url,
