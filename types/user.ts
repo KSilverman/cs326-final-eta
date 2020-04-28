c61932f38be87b9665466dc9be571dfe8f07e0ae
Devon Endicott <dephurites@gmail.com>
2020-04-28 15:54:08 -0400

    basic unvalidated user get/put, hashing

--- types/user.ts
+++ types/user.ts
@@ -1,9 +1,48 @@
-export class User {
-  private id : number;
-  private username : string;
+const bcrypt = require('bcrypt')
 
-  constructor(id: number, username: string) {
+export class User {
+  readonly id : number;
+  public username : string;
+  private hash : string;
+
+  private static saltRounds : number = 10;
+
+  constructor(id: number, username: string, hash: string) {
     this.id = id;
     this.username = username;
+    this.hash = hash;
+  }
+
+  public static async createHash(password : string) : Promise<string> {
+    let hash : string = await new Promise((resolve : any, reject : any) => {
+      bcrypt.hash(password, User.saltRounds, (err : any, hash : string) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(hash);
+      })
+    });
+
+    return hash;
+  }
+
+  public async checkHash(password : string) : Promise<boolean> {
+    let match : any = new Promise((resolve : any, reject : any) => {
+      bcrypt.compare(password, this.hash, (err : any, res : boolean) => {
+        if (err) reject(err);
+
+        resolve(res);
+      })
+    });
+
+    return match as boolean;
+  }
+
+  public async objectify() : Promise<any> {
+    return {
+      id: this.id,
+      username: this.username,
+    };
   }
 }
