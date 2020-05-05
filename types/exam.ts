98ae37f8a0cc8f522005379cded9b24a30876e88
Devon Endicott <dephurites@gmail.com>
2020-05-04 21:09:54 -0400

    exam class

--- types/exam.ts
+++ types/exam.ts
@@ -1,3 +1,23 @@
 export class Exam {
-  
+
+  readonly id : number;
+  readonly uid : number;
+  public name : string;
+  public calendarData : object;
+
+  public Exam(id : number, uid : number, name : string, calendarData : object) {
+    this.id = id;
+    this.uid = uid;
+    this.name = name;
+    this.calendarData = calendarData;
+  }
+
+  public objectify() : object {
+    return {
+      id: id,
+      uid: uid,
+      name: name,
+      calendarData: calendarData
+    }
+  }
 }
